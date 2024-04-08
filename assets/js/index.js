5af00eaf89b6e5f07b1b45a609268387ba52047f
Bryan Melo <bryan.melo@gmail.com>
2024-04-08 17:50:55 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,6 +9,10 @@ function sendToWhatsapp() {
     let horario_inicio = document.getElementById('horario_inicio').value;
     let telefone = document.getElementById('telefone').value;
 
+
+    //formatando data para padrao pt-br
+    let data_festa_formatada = data_festa.toLocaleDateString('pt-BR');
+
     var url = "https://wa.me/" + number + "?text=" +
         "Nome: " + name + "%0a" +
         "E-mail: " + email + "%0a" +
